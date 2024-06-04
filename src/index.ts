965cda3885ed5ed5c612b1d917079ff27b06c692
Daniil <danrezni.pg@gmail.com>
2024-06-04 21:06:40 +0200

    feat: CRUD operations examples

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express, { Request, Response } from "express";
-import e from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const products = [
+let products = [
   { id: 1, title: "tomato" },
   { id: 2, title: "orange" },
 ];
-const addresses = [{ value: "a" }, { value: "b" }];
+const addresses = [
+  { id: 1, value: "address 1" },
+  { id: 2, value: "address 2" },
+];
+// Middleware
+// Добавили для того, чтобы body конвертировался из байтов в JSON
+// deprecated
+const parserMiddleware = bodyParser({});
+app.use(parserMiddleware);
 
+// GET запросы -----------------------------------------------------
 // Здесь добавили query параметры
 app.get("/products", (req: Request, res: Response) => {
   if (req.query.title) {
@@ -19,9 +28,9 @@ app.get("/products", (req: Request, res: Response) => {
     res.send(products);
   }
 });
-
-// Вот в express  создается динамический роут с URI параметром
+// Вот так в express  создается динамический роут с URI параметром
 app.get("/products/:id", (req: Request, res: Response) => {
+  // Параметры из URI всегда имеют тип string
   const product = products.find((p) => +req.params.id === p.id);
   if (product) {
     res.send(product);
@@ -31,11 +40,43 @@ app.get("/products/:id", (req: Request, res: Response) => {
     res.send(404);
   }
 });
-
 app.get("/addresses", (req: Request, res: Response) => {
   res.send(addresses);
 });
 
+// DELETE запросы -----------------------------------------------------
+app.delete("/products/:id", (req: Request, res: Response) => {
+  let status = 404;
+  products = products.filter((p) => {
+    if (p.id !== +req.params.id) {
+      status = 204;
+      return true;
+    }
+  });
+  res.send(status);
+});
+
+// POST запросы -----------------------------------------------------
+app.post("/products", (req: Request, res: Response) => {
+  const newProduct = { id: +new Date(), title: req.body.title };
+  products.push(newProduct);
+  // Добавляем статус + возращаем созданную сущность
+  res.status(201).send(newProduct);
+});
+
+// PUT запросы -----------------------------------------------------
+app.put("/products/:id", (req: Request, res: Response) => {
+  // Параметры из URI всегда имеют тип string
+  const product = products.find((p) => +req.params.id === p.id);
+  if (product) {
+    product.title = req.body.title;
+    res.send(product);
+  } else {
+    // Фишка express - если кинуть сюда код ошибки, то он будет возвращать
+    // на клиент такой статус код (deprecated)
+    res.send(404);
+  }
+});
 // Выполняется при запуске приложения
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
